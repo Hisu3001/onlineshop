4d9c7a813aa64c979deb3dfb88aa0d66b9cb7507
Hisu3001 <minhhieu321t@gmail.com>
2024-11-01 14:42:17 +0700

    feat(project): create new

---
+++ frontend/src/utils/api/variables.js
@@ -0,0 +1,8 @@
+export const variables = {
+    BASE_URL: "http://api-online-shop.com/api/",
+    USER_API: "http://api-online-shop.com/api/user",
+    PRODUCT_API: "http://api-online-shop.com/api/product",
+    ORDER_API: "http://api-online-shop.com/api/order",
+    PRODUCTSIZE_API: "http://api-online-shop.com/api/productsize",
+    ORDERITEM_API: "http://api-online-shop.com/api/orderitem",
+}
